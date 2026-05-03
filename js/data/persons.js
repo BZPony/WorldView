347efa5c52739aaae3842de30c6b11f9cf4fed16
BZPony <1092840100@qq.com>
2026-05-03 15:57:43 +0800

    new file:   .DS_Store new file:   css/base.css 	new file:   css/context-menu.css 	new file:   css/sidebar.css 	new file:   css/timeline.css 	new file:   js/data/persons.js 	new file:   js/icons/svg-icons.js 	modified:   myMap.html 	new file:   myMap.html.zip

---
+++ js/data/persons.js
@@ -0,0 +1,89 @@
+const persons = [
+    {
+        id: 1,
+        name: "张三",
+
+        timeline: [
+            { lat: 30, lng: 110, time: 0 },
+            { lat: 32, lng: 115, time: 20 },
+            { lat: 35, lng: 120, time: 40 },
+            { lat: 40, lng: 116, time: 80 }
+        ],
+        color: "#4f454f",
+        iconUrl: null,
+        iconSize: [32, 32],
+
+        tags: [String],
+        marker: null,
+        polyline: null
+    },
+    {
+        id: 2,
+        name: "暮光闪闪",
+
+        timeline: [
+            { lat: 31, lng: 120, time: -200 },
+            { lat: 33, lng: 118, time: 0 },
+            { lat: 36, lng: 122, time: 50 },
+            { lat: 38, lng: 119, time: 150 }
+        ],
+        color: "#ff4d4f",
+        iconUrl: null,
+        iconSize: [32, 32],
+
+        tags: [String],
+        marker: null,
+        polyline: null
+    },
+    {
+        id: 3,
+        name: "夜神月",
+        timeline: [
+            { lat: 28, lng: 115, time: -50 },
+            { lat: 30, lng: 118, time: 0 },
+            { lat: 34, lng: 117, time: 30 },
+            { lat: 37, lng: 121, time: 70 }
+        ],
+        color: "#347290",
+        iconUrl: null,
+        iconSize: [32, 32],
+
+        tags: [String],
+        marker: null,
+        polyline: null
+    },
+    {
+        id: 4,
+        name: "尤莉",
+        timeline: [
+            { lat: 29, lng: 112, time: -100 },
+            { lat: 31, lng: 114, time: -20 },
+            { lat: 33, lng: 116, time: 10 },
+            { lat: 36, lng: 118, time: 60 }
+        ],
+        color: "#832900",
+        iconUrl: null,
+        iconSize: [32, 32],
+
+        tags: [String],
+        marker: null,
+        polyline: null
+    },
+    {
+        id: 5,
+        name: "利维亚的杰洛特",
+        timeline: [
+            { lat: 32, lng: 110, time: -150 },
+            { lat: 35, lng: 113, time: -50 },
+            { lat: 38, lng: 116, time: 50 },
+            { lat: 41, lng: 119, time: 150 }
+        ],
+        color: "#247290",
+        iconUrl: null,
+        iconSize: [32, 32],
+
+        tags: [String],
+        marker: null,
+        polyline: null
+    }
+];
